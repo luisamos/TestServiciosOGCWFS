2dc5f6c0d8a5a2a88414c5cead33a636f9bfefce
Luis Amos <luisamos7@gmail.com>
2021-05-28 19:18:50 -0500

    Update MINAGRI.py

--- MINAGRI.py
+++ MINAGRI.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Parametros
 ruta_descarga = r'/apps/python'
 tipo_geometria = 'polygon'     # options: point, line, polygon
-url_wfs = 'https://geo.minagri.gob.pe/arcgis/services/servicios_ogc/Peru_midagri_1501_tumbes/MapServer/WFSServer?VERSION=1.0.0&MAXFEATURES=7667'
+url_wfs = 'https://geo.minagri.gob.pe/arcgis/services/servicios_ogc/Peru_midagri_1501_ucayali/MapServer/WFSServer?VERSION=1.0.0&MAXFEATURES=12337'
 #url_wfs = 'https://geo.minagri.gob.pe/arcgis/services/servicios_ogc/Peru_midagri_1501_puno/MapServer/WFSServer?'
 
 driver_wfs = ogr.GetDriverByName('WFS')
